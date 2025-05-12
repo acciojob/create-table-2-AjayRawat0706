dbcccd57b0901c1e56e788dc25617cf9b9b8e8af
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-13 00:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function createTable() {
-    //Write your code here
-  
+function createTable(data,columns) {
+	let rn=prompt("Input number of rows");
+	let cn=prompt("Input number of columns");
+	let table=document.getElementById("myTable");
+	table.innerHTML='';
+	//create table rows
+	let tbody=document.createElement('tbody');
+	table.appendChild(tbody);
+for(let i=0;i<=1;i++){
+	let row=document.createElement('tr');
+	tbody.appendChild(row);
+	for(let j=0;j<=1;j++){
+		let cell=document.createElement('td');
+		cell.textContent=`Row-${i} Column-${j}`;
+	    row.appendChild(cell);
+	}
+}
+	
 }
